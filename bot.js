7c1616f3d5e15d4c1d298a15c4aeab620c142f04
matchashiba <recr38@gmail.com>
2020-04-30 23:34:26 +0000

    Added some message tests to bot.js

--- bot.js
+++ bot.js
@@ -12,3 +12,20 @@ if(process.env.NODE_ENV === 'production'){
 } else {
   bot = new Telegram_Bot(token, {polling: true});
 }
+
+bot.on('message', (msg) => {
+  var hi = 'hi';
+  if(msg.text.toString().toLowerCase().indexOf(hi) === 0){
+    bot.sendMessage(msg.chat.id, `Beep boop, Konnichiwa ${msg.from.first_name}!`);
+  }
+
+  var bye = 'bye';
+  if(msg.text.toString().toLowerCase().includes(bye)){
+    bot.sendMessage(msg.chat.id, "Okay, see ya");
+  }
+
+  var who = 'who are you?';
+  if(msg.text.toString().toLowerCase().indexOf(who) === 0){
+    bot.sendMessage(msg.chat.id, 'Brrrrzzzt. I am Matcha Bot. The bot version of Matcha Shiba');
+  }
+});
